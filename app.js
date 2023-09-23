32813aade1d7e55e10be1b729205eaf298ce6822
B2012105diemkieu <kieuq20021994@gmail.com>
2023-09-23 17:22:13 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const cors = require("cors");
 
 const app = express();
 
+//add router
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
+
+//use router
+app.use("/api/contacts", contactsRouter);
+
 
 module.exports = app;
